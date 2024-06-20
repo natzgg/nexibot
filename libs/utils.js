4b51d6a70de2bb49758b68439ff954b05789d046
natzgg <natzcunanan@gmail.com>
2024-06-20 14:50:46 +0900

    Fix AIResponse when have quotation and making one words into two words by adding space between

--- libs/utils.js
+++ libs/utils.js
@@ -6,9 +6,17 @@ export const parseOpenAIResponse = (response) => {
   // Split the response by "0:" and filter out empty strings
   const words = response.split("0:").filter(Boolean);
   // Remove double quotes from each word
-  const cleanWords = words.map((word) => word.replace(/"/g, "").trim());
+  const cleanWords = words.map((word) => word.replace(/"\s*|\s*"/g, "").trim());
 
   // Join the clean words to form a sentence
-  const sentence = cleanWords.join(" ").replace(/\s+([!?,.:;'"])/g, "$1");
-  return sentence.trim(); // Remove any leading or trailing whitespace
+  const sentence = cleanWords
+    .map((word, index, array) =>
+      index < array.length - 1 && /[!?,.:;'"]$/.test(word) ? word.trim() : word
+    )
+    .join(" ")
+    .replace(/\s+([!?,.:;'"])/g, "$1");
+
+  //Fix the quotation is being replaced by "\"
+  const qSentence = sentence.replace(/\\n/g, "\n").replace(/\\/g, '"');
+  return qSentence.trim(); // Remove any leading or trailing whitespace
 };
